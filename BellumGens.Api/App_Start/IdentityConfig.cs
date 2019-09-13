3909f1c377fbbb684c3a9dc3a4e0d7508cb1c5da
Konstantin Dinev <kdinev@mail.bw.edu>
2019-09-13 16:24:17 -0400

    Fixing a critical issue with username not allowing special chars

--- BellumGens.Api/App_Start/IdentityConfig.cs
+++ BellumGens.Api/App_Start/IdentityConfig.cs
@@ -22,11 +22,11 @@ namespace BellumGens.Api
         {
             var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<BellumGensDbContext>()), new EmailService());
 			// Configure validation logic for usernames
-			//manager.UserValidator = new UserValidator<ApplicationUser>(manager)
-			//{
-			//	AllowOnlyAlphanumericUserNames = false,
-			//	RequireUniqueEmail = false
-			//};
+			manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+			{
+				AllowOnlyAlphanumericUserNames = false,
+				RequireUniqueEmail = false
+			};
 			//// Configure validation logic for passwords
 			//manager.PasswordValidator = new PasswordValidator
 			//{
