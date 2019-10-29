ddc711e1c255bf83684c8808e9545704edbf6811
Konstantin Dinev <kdinev@mail.bw.edu>
2019-10-29 18:22:19 +0200

    This should dramatically improve search response time.

--- BellumGens.Api/Models/ViewModels/SearchResultViewModel.cs
+++ BellumGens.Api/Models/ViewModels/SearchResultViewModel.cs
@@ -6,7 +6,7 @@ namespace BellumGens.Api.Models
 	{
 		public List<CSGOTeam> Teams { get; set; } = new List<CSGOTeam>();
 		public List<CSGOStrategy> Strategies { get; set; } = new List<CSGOStrategy>();
-		public UserStatsViewModel[] Players { get; set; }
+        public List<UserStatsViewModel> Players { get; set; } = new List<UserStatsViewModel>();
         public UserStatsViewModel SteamUser { get; set; }
 	}
 }
