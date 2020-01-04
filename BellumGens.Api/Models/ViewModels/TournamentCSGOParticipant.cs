310a8690ec34396ff1a6d9102597069c3f9f0490
Konstantin Dinev <kdinev@mail.bw.edu>
2020-01-04 12:11:28 +0200

    adding endpoints for sc2 and csgo participants

---
+++ BellumGens.Api/Models/ViewModels/TournamentCSGOParticipant.cs
@@ -0,0 +1,16 @@
+﻿namespace BellumGens.Api.Models
+{
+    public class TournamentCSGOParticipant
+    {
+        public TournamentCSGOParticipant(TournamentApplication application)
+        {
+            UserId = application.UserId;
+            Team = new CSGOTeamSummaryViewModel(application.Team);
+            State = application.State;
+        }
+
+        public string UserId { get; set; }
+        public CSGOTeamSummaryViewModel Team { get; set; }
+        public TournamentApplicationState State { get; set; }
+    }
+}
