2b0041eb457d9c445a4d3c3b05e7392aedda45c0
Konstantin Dinev <kdinev@mail.bw.edu>
2020-01-11 10:46:59 +0200

    adding admin controller

---
+++ BellumGens.Api/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BellumGens.Api.Controllers
+{
+    [Authorize(Roles = "admin")]
+    [RoutePrefix("api/Admin")]
+    public class AdminController : BaseController
+    {
+        [AllowAnonymous]
+        [Route("AppAdmin")]
+        public bool GetUserIsAdmin()
+        {
+            return User.Identity.IsAuthenticated && User.IsInRole("admin");
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("CreateRole")]
+        public IHttpActionResult CreateRole(string rolename)
+        {
+            _dbContext.Roles.Add(new IdentityRole()
+            {
+                Name = rolename
+            });
+
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        [AllowAnonymous]
+        [Route("Roles")]
+        public IHttpActionResult GetRoles()
+        {
+            return Ok(_dbContext.Roles.ToList());
+        }
+    }
+}
