78e268df5a2ab17b8570edc4d83ec8c674bd94e0
Konstantin Dinev <kdinev@mail.bw.edu>
2020-01-13 17:56:03 +0200

    temporarily disabling some endpoints

--- BellumGens.Api/Controllers/AdminController.cs
+++ BellumGens.Api/Controllers/AdminController.cs
@@ -17,44 +17,44 @@ namespace BellumGens.Api.Controllers
             return UserManager.IsInRole(SteamServiceProvider.SteamUserId(User.Identity.GetUserId()), "admin");
         }
 
-        [AllowAnonymous]
-        [HttpGet]
-        [Route("CreateRole")]
-        public async Task<IHttpActionResult> CreateRole(string rolename)
-        {
-            var result = await RoleManager.CreateAsync(new IdentityRole() { Name = rolename }).ConfigureAwait(false);
-            if (result.Succeeded)
-            {
-                return Ok();
-            }
-            return BadRequest();
-        }
+        //[AllowAnonymous]
+        //[HttpGet]
+        //[Route("CreateRole")]
+        //public async Task<IHttpActionResult> CreateRole(string rolename)
+        //{
+        //    var result = await RoleManager.CreateAsync(new IdentityRole() { Name = rolename }).ConfigureAwait(false);
+        //    if (result.Succeeded)
+        //    {
+        //        return Ok();
+        //    }
+        //    return BadRequest();
+        //}
 
-        [AllowAnonymous]
-        [Route("Roles")]
-        public IHttpActionResult GetRoles()
-        {
-            return Ok(RoleManager.Roles.Select(r => r.Name).ToList());
-        }
+        //[AllowAnonymous]
+        //[Route("Roles")]
+        //public IHttpActionResult GetRoles()
+        //{
+        //    return Ok(RoleManager.Roles.Select(r => r.Name).ToList());
+        //}
 
-        [AllowAnonymous]
-        [Route("Users")]
-        public IHttpActionResult GetUsers()
-        {
-            return Ok(UserManager.Users.ToList());
-        }
+        //[AllowAnonymous]
+        //[Route("Users")]
+        //public IHttpActionResult GetUsers()
+        //{
+        //    return Ok(UserManager.Users.ToList());
+        //}
 
-        [AllowAnonymous]
-        [HttpGet]
-        [Route("AddUserRole")]
-        public async Task<IHttpActionResult> AddUserRoles(string userid, string role)
-        {
-            IdentityResult result = await UserManager.AddToRoleAsync(userid, role).ConfigureAwait(false);
-            if (result.Succeeded)
-            {
-                return Ok("Ok");
-            }
-            return BadRequest("Something went wrong");
-        }
+        //[AllowAnonymous]
+        //[HttpGet]
+        //[Route("AddUserRole")]
+        //public async Task<IHttpActionResult> AddUserRoles(string userid, string role)
+        //{
+        //    IdentityResult result = await UserManager.AddToRoleAsync(userid, role).ConfigureAwait(false);
+        //    if (result.Succeeded)
+        //    {
+        //        return Ok("Ok");
+        //    }
+        //    return BadRequest("Something went wrong");
+        //}
     }
 }
