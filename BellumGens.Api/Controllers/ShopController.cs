7097074d314683ef53dfe9c9a87142d7ff202642
Konstantin Dinev <kdinev@mail.bw.edu>
2020-04-10 22:23:35 +0300

    feat(*): adding setup for the shop

---
+++ BellumGens.Api/Controllers/ShopController.cs
@@ -0,0 +1,37 @@
+﻿using BellumGens.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BellumGens.Api.Controllers
+{
+    [RoutePrefix("api/Shop")]
+    public class ShopController : BaseController
+    {
+        [HttpPost]
+        [Route("Order")]
+        public IHttpActionResult SubmitOrder(JerseyOrder order)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.JerseyOrders.Add(order);
+
+                try
+                {
+                    _dbContext.SaveChanges();
+                }
+                catch (DbUpdateException e)
+                {
+                    System.Diagnostics.Trace.TraceError("Order update exception: " + e.Message);
+                    return BadRequest("Something went wrong...");
+                }
+                return Ok(order);
+            }
+            return BadRequest("Order couldn't be validated");
+        }
+    }
+}
